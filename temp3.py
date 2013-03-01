5945e050b908e269931d000aaddfd02d261381f4
Pranav Rao <pnpranavrao@gmail.com>
2013-03-02 05:06:21 +0530

    Cleanup

--- temp3.py
+++
@@ -1,4 +0,0 @@
-from github.temp2 import *
-gh = GitHub(username='pnpranavrao', password='studio15')
-L = gh.repos('pnpranavrao')
-print  L
---
+++ testfile.py
@@ -0,0 +1,32 @@
+import json
+from github.githubapi import *
+
+#What do I Need to do?
+
+#* Authenticate the user
+#* When given a file name(figure out how) in the argument, return the json.
+#* When forks of a file are asked, list everything. 
+#* Create a fork 
+#* Given a fork and master, supply the diff properly. 
+#* Some mechanism to show which lines were written by whom. 
+
+###Later
+#* Proper stats.
+
+def get_contents(user='yahoohackathon',repo='testrepo'):
+	""" Default username is 'yahoohackathon', and default repo will be
+	'testrepo' """
+	gh = GitHub(username=user, password='123yahoo')
+	query = gh.repos(user)(repo).readme
+	result = query.get()
+	
+	return_dict = {}
+	return_dict["user"] = user
+	for k,v in result.iteritems():
+		if k in ["name","content"]:
+			return_dict[k] = v
+	print json.dumps(return_dict)
+
+if __name__=='__main__':
+	get_contents()
+
